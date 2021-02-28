4736ff7dce2dc402b88d06c4ead5576e1b50a3fd
Joe Albert <joe.r.albert@gmail.com>
2021-02-28 23:26:08 +0000

    Lab meeting changes

--- lab3/lab3_src.c
+++ lab3/lab3_src.c
@@ -5,20 +5,51 @@
 */
 
 #include<stdio.h>
+#include<wiringPi.h>
+#include<unistd.h>
+
+void turn_on_leds(int max_led);
 
 int main(void)
 {
     FILE * load_file;
     char line[256];
+    float load_avg;
 
-    load_file = fopen("/proc/loadavg", "r");
-
-    fgets(line, sizeof line, load_file);
-
-    printf("%s", line);
-
-    fclose(load_file);
+    // setup for pin output
+    wiringPiSetup();
+    for (int pin = 0; pin <= 7; pin++)
+        pinMode(pin, OUTPUT);
     
+    while(1)
+    {
+        load_file = fopen("/proc/loadavg", "r");
 
+        fgets(line, sizeof line, load_file);
+
+        sscanf(line, "%f", &load_avg);
+
+        printf("%d", load_avg);
+
+        fclose(load_file);
+ 
+        if (load_avg >=4)
+            turn_on_leds(8);
+        else if load_avg
+        
+        sleep(1);
+    }
+    return 0;
+}
+
+void turn_on_leds(int max_led)
+{
+    for (int pin = 0; pin < 8; pin++)
+    {
+        if (pin + 1 <= max_led)
+            digitalWrite(pin, HIGH);
+        else
+            digitalWrite(pin, LOW);
+    }
 }
 
