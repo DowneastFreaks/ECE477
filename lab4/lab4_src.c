59dcb73b522282f59e1fc4126cea702c20a5856a
Joe Albert <joe.r.albert@gmail.com>
2021-03-09 23:28:19 +0000

    Expand docstrings and use bitshifting instead of *= 2 , /= 2 statements

--- lab4/lab4_src.c
+++ lab4/lab4_src.c
@@ -1,6 +1,12 @@
 /*
 * Lab 4
-* ECE477 Alyssa Wardwell and Joseph Albert
+* ECE477
+* @author Alyssa Wardwell
+* @author Joseph Albert
+*
+* Lit LED moves across 8 LEDs
+* Switch A increases speed, Switch B decreases speed, A + B closes program
+* When max and min speed are reached, maxed switch will reverse direction of LED movement.
 */
 
 
@@ -21,7 +27,9 @@ typedef struct led_parameters
     unsigned char value;
 } LedParameters;
 
-// Function Prototype
+// Function Prototypes
+void switch_a_pressed();
+void switch_b_pressed();
 void init(void);
 void display_leds(unsigned char value);
 bool get_bit(int number, int index);
@@ -34,7 +42,57 @@ LedParameters update_led_value(LedParameters param);
 volatile bool pin_a_state = 0;
 volatile bool pin_b_state = 0;
 
-// Button interrupts
+
+int main()
+{
+    LedParameters led_settings = {1, 1024, 0};  // direction, refresh_rate, value
+
+    init();
+
+
+    while(1)
+    {
+        // update direction and refresh rate if button is pushed
+    	if (pin_a_state || pin_b_state)
+            led_settings = (pin_a_state) ? update_parameters(led_settings, SWITCH_PIN_A): update_parameters(led_settings, SWITCH_PIN_B);
+
+
+        // update value to be displayed on LEDs
+        led_settings = update_led_value(led_settings);
+
+        display_leds(led_settings.value);
+
+    	delay(led_settings.refresh_rate);
+    }
+}
+
+
+/*
+* One time setup of hardware
+*/
+void init(void)
+{
+    wiringPiSetup();
+
+    pinMode(SWITCH_PIN_A, INPUT);
+    pinMode(SWITCH_PIN_B, INPUT);
+
+    pullUpDnControl(SWITCH_PIN_A, PUD_UP);
+    pullUpDnControl(SWITCH_PIN_B, PUD_UP);
+
+    wiringPiISR(SWITCH_PIN_A, INT_EDGE_FALLING, &switch_a_pressed);
+    wiringPiISR(SWITCH_PIN_B, INT_EDGE_FALLING, &switch_b_pressed);
+
+    for (int pin = 0; pin < 8; pin++)
+        pinMode(pin, OUTPUT);
+}
+
+
+/*
+* Interrupt triggered when switch A pressed
+* if low signal is confirmed set pin_a_state to true
+* shuts off method if switch B is also pressed
+*/
 void switch_a_pressed()
 {
     bool event_triggered = 0;
@@ -53,6 +111,12 @@ void switch_a_pressed()
     }
 }
 
+
+/*
+* Interrupt triggered when switch B pressed
+* if low signal is confirmed set pin_b_state to true
+* shuts off method if switch A is also pressed
+*/
 void switch_b_pressed()
 {
     bool event_triggered;
@@ -71,46 +135,10 @@ void switch_b_pressed()
     }
 }
 
-// main function
-int main()
-{
-    LedParameters led_settings = {1, 1024, 0};
 
-    init();
-
-
-    while(1)
-    {
-        // Check to update settings
-    	if (pin_a_state || pin_b_state)
-            led_settings = (pin_a_state) ? update_parameters(led_settings, SWITCH_PIN_A): update_parameters(led_settings, SWITCH_PIN_B);
-
-
-        led_settings = update_led_value(led_settings);
-
-        display_leds(led_settings.value);
-
-    	delay(led_settings.refresh_rate);
-    }
-}
-
-void init(void)
-{
-    wiringPiSetup();
-
-    pinMode(SWITCH_PIN_A, INPUT);
-    pinMode(SWITCH_PIN_B, INPUT);
-
-    pullUpDnControl(SWITCH_PIN_A, PUD_UP);
-    pullUpDnControl(SWITCH_PIN_B, PUD_UP);
-
-    wiringPiISR(SWITCH_PIN_A, INT_EDGE_FALLING, &switch_a_pressed);
-    wiringPiISR(SWITCH_PIN_B, INT_EDGE_FALLING, &switch_b_pressed);
-
-    for (int pin = 0; pin < 8; pin++)
-        pinMode(pin, OUTPUT);
-}
-
+/*
+* Turn on LEDs to match binary representation of value
+*/
 void display_leds(unsigned char value)
 {
     for (int i = 7; i >= 0; i--)
@@ -123,9 +151,13 @@ void display_leds(unsigned char value)
 }
 
 
+/*
+* Update direction and refresh_rate of LEDs based on current values and
+* which switch was pushed.
+*/
 LedParameters update_parameters(LedParameters param, unsigned switch_pin)
 {
-    // Reset pins
+    // Reset global variables to 0
     pin_a_state = pin_b_state = 0;
 
     if (switch_pin == SWITCH_PIN_A)
@@ -133,7 +165,7 @@ LedParameters update_parameters(LedParameters param, unsigned switch_pin)
         if (32 == param.refresh_rate)
             param.direction = !param.direction;
         else
-            param.refresh_rate /= 2;
+            param.refresh_rate >>= 1;
 
     }
     else
@@ -141,12 +173,16 @@ LedParameters update_parameters(LedParameters param, unsigned switch_pin)
         if (1024 == param.refresh_rate)
             param.direction = !param.direction;
         else
-            param.refresh_rate *= 2;
+            param.refresh_rate <<= 1;
     }
 
     return param;
 }
 
+
+/*
+* Uses current LedParameters to determine the next value in the sequence
+*/
 LedParameters update_led_value(LedParameters param)
 {
     unsigned char led_value = param.value;
@@ -168,6 +204,10 @@ LedParameters update_led_value(LedParameters param)
 
 }
 
+
+/*
+* Returns the state of a bit in a number
+*/
 bool get_bit(int number, int index)
 {
     unsigned flag = 1;
@@ -176,6 +216,7 @@ bool get_bit(int number, int index)
 
     return number & flag;
 }
+
 
 /*
 * Function to debounce signal from button
@@ -190,9 +231,8 @@ bool confirm_low_signal(unsigned pin)
         buffer |= digitalRead(pin);
 
         if (i > 7 && 0x00 == buffer)
-        {
             return 1;
-        }
+
         delay(3);
     }
 
