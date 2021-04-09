e4becc7f7b1e148f385f70395a2c068489e886ce
Joe Albert <joe.r.albert@gmail.com>
2021-04-09 21:52:24 +0100

    Lab6 Part A COMPLETE

--- lab6/lab6_read_avr.c
+++ lab6/lab6_read_avr.c
@@ -6,19 +6,28 @@
 int main()
 {
     int fd;
-    
+    char write_value[100];
+    FILE * serial_fp = NULL;
+    FILE * log_fp = NULL;
+    float reading = 0;
+
     if ((fd = serialOpen("/dev/ttyS0",9600)) < 0)
     {
         fprintf(stderr, "Unable to open serial device: %s", strerror(errno));
         return 1;
     }
 
+    serial_fp = fdopen(fd, "r");
+
     serialPrintf(fd, "Start\n\r");
 
     while (1)
     {
-        putchar(serialGetchar(fd));
-        fflush(stdout);
+        fscanf(serial_fp, "The power rail is approximately %fV\n", &reading );
+        log_fp = fopen("./rail_voltages.dat", "a");
+        fprintf(log_fp, "%.6f\n\r", reading);
+        fclose(log_fp);
+        
     }
 
 }
